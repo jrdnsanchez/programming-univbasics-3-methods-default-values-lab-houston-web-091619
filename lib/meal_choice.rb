14d5ef1402672f24f52ff1fa1a29921d9aa95bff
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-09 20:18:11 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,3 @@
 def meal_choice (veg1, veg2, protein = "meat")
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
-def meal_choice ("omnivorous", "vegetarian", "vegan")
-  return "What a nutritious meal!"
-end
