4f7cc00cfc8ec3d33f3c15047e5522c9d10f148d
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-09 20:38:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,12 @@
 def meal_choice (veg1, veg2, protein = "meat")
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
+escribe '#meal_choice' do
+  it 'should default to meat' do
+    expect(meal_choice).to eq("meat")
+  end
+
+  it 'should allow you to set a meal' do
+    expect(meal_choice("vegan")).to eq("vegan")
+  end
+end
